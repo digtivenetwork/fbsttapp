aac22091fb1b8a525b0238e0f781b9b743af58aa
aderahmadw <aderahmad555@gmail.com>
2023-08-25 18:45:26 +0700

    commit desain page welcome

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul class="bg-white mx-auto text-center flex space-x-2">
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">«</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">‹</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">1</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-blue-500 bg-blue-500 w-full h-full rounded-full text-white hover:bg-blue-600 transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">2</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">3</button></li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">4</button></li>
+        <li class="w-8 h-8 rounded-full hidden md:inline"><span class="">…</span></li><li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">›</button></li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">»</button>
+        </li>
+    </ul>
+</template>
