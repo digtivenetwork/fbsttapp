5d929e35dfe85f621662dff0d3749f1f6f5940b2
Surya Abdul <34419247+3MTape@users.noreply.github.com>
2023-08-26 10:01:58 +0700

    Revert "Adjustment For Admin Page"

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,40 +1,25 @@
-<script setup>
-import { defineComponent } from "vue";
-import { Link } from '@inertiajs/vue3';
-
-// defineComponent({
-//     components: {
-//         Link,
-//     },
-//     props: {
-//         links: Object,
-//     },
-// });
-const props = defineProps({
-    links:{
-        type:Array
-    }
-});
-</script>
 <template>
-    
-    <div v-if="links.length > 3">
-        <div class="flex flex-wrap mt-8">
-            <template v-for="(link, key) in links" :key="key">
-                <div
-                    v-if="link.url === null"
-                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
-                    v-html="link.label"
-                />
-
-                <Link
-                    v-else
-                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-primary focus:text-primary"
-                    :class="{ 'bg-blue-700 text-white': link.active }"
-                    :href="link.url"
-                    v-html="link.label"
-                />
-            </template>
-        </div>
-    </div>
+    <ul class="bg-white mx-auto text-center flex space-x-2">
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">«</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">‹</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">1</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-blue-500 bg-blue-500 w-full h-full rounded-full text-white hover:bg-blue-600 transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">2</button>
+        </li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">3</button></li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">4</button></li>
+        <li class="w-8 h-8 rounded-full hidden md:inline"><span class="">…</span></li><li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">›</button></li>
+        <li class="w-8 h-8 rounded-full">
+            <button class="border border-gray-200 hover:bg-blue-100 hover:border-blue-100 rounded-full w-full h-full transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50">»</button>
+        </li>
+    </ul>
 </template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,7 +6,6 @@ import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
-import FBSLogo from '/resources/images/Logo_FBS-Wide.png';
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -22,22 +21,17 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
-                                    <img class="h-8 w-auto" :src="FBSLogo" alt="FBS Triumph" />
-                                    <!-- <ApplicationLogo
+                                    <ApplicationLogo
                                         class="block h-9 w-auto fill-current text-gray-800"
-                                    /> -->
+                                    />
                                 </Link>
                             </div>
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Claimed Prize
+                                    Dashboard
                                 </NavLink>
-                                <NavLink :href="route('import.index')" :active="route().current('import.index')">
-                                    Import
-                                </NavLink>
-                                
                             </div>
                         </div>
 
